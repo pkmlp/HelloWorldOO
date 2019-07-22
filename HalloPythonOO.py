5f77a2d6ddcb68291f658ccc088025cff86f2777
pkmlp <pkmlp@pkmlp.ch>
2019-07-22 13:28:18 +0200

    updated HellWorld OOP

--- HalloPythonOO.py
+++ HalloPythonOO.py
@@ -1,13 +1,13 @@
 
 class HalloPythonOO:
-  
+
     def __init__(self):
         self.halloWeltText = "Hallo Python wirklich Objekt-Orientiert !!!"
-  
+
     def sagHalloWelt(self):
         print(self.halloWeltText)
- 
- 
+
+
 if __name__ == "__main__":
     halloPython = HalloPythonOO()
     halloPython.sagHalloWelt()
