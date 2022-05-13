c91dd91cfc765b3bf08a4acd518524447559115c
pkmlp <pkmlp@pkmlp.ch>
2022-05-13 12:02:12 +0200

    Some Updates

--- HalloPythonOO.py
+++ HalloPythonOO.py
@@ -1,13 +1,22 @@
 
 class HalloPythonOO:
-  
+    """
+
+    Definition/Bauplan der Objekte diese Klasse
+
+    Parameter: keine
+
+   """
+
     def __init__(self):
+        '''Definition des Hallo Textes'''
         self.halloWeltText = "Hallo Python wirklich Objekt-Orientiert !!!"
-  
+
     def sagHalloWelt(self):
+        '''Ausgabe des Hallo Textes'''
         print(self.halloWeltText)
- 
- 
+
+
 if __name__ == "__main__":
     halloPython = HalloPythonOO()
     halloPython.sagHalloWelt()
